8d11d086079aae5687f8661007a04f9036d25b5e
yoavberko <144139207+yoavberko@users.noreply.github.com>
2025-08-14 13:32:57 +0300

    Update app.py

--- first-app/app.py
+++ first-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     version = "v1.0.0"  # Change this to test deployments
-    return f"Hello, World! Running version {version}."
+    return f"Hello, World! Running version {version}. also, goodbye!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
